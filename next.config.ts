c40d2dda8acf85f403a30bfa969f21f6b0a6e35e
Sagar <sagarmugesh10@gmail.com>
2026-09-09 20:48:49 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,21 +3,59 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   turbopack: {},
   reactCompiler: true,
+
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: "/(.*)",
         headers: [
-          { key: 'X-Frame-Options', value: 'DENY' },
-          { key: 'X-Content-Type-Options', value: 'nosniff' },
-          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
-          { key: 'Permissions-Policy', value: 'geolocation=(self), camera=(self), microphone=()' },
-          { key: 'X-XSS-Protection', value: '1; mode=block' },
-          { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value: "geolocation=(self), camera=(self), microphone=()",
+          },
+          {
+            key: "X-XSS-Protection",
+            value: "1; mode=block",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains",
+          },
         ],
       },
-    ]
+    ];
   },
 };
 
 export default nextConfig;
+
+
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   turbopack: {},
+//   reactCompiler: true,
+//   async headers() {
+//     return [
+//       {
+//         source: '/(.*)',
+//         headers: [
+//           { key: 'X-Frame-Options', value: 'DENY' },
+//           { key: 'X-Content-Type-Options', value: 'nosniff' },
+//           { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+//           { key: 'Permissions-Policy', value: 'geolocation=(self), camera=(self), microphone=()' },
+//           { key: 'X-XSS-Protection', value: '1; mode=block' },
+//           { key: 'Strict-Transport-Security', value: 'max-age=31536000; includeSubDomains' },
+//         ],
+//       },
+//     ]
+//   },
+// };
+
+// export default nextConfig;
